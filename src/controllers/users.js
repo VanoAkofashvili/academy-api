db46df8e6657b919be2cdc026e000bd9469c0c22
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2022-09-08 19:02:00 +0400

    test

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,14 +1,28 @@
 const { transformSuccess } = require('../utils');
 const { faker } = require('@faker-js/faker');
+const jwt = require('jsonwebtoken');
 
-const user = {
-  id: faker.database.mongodbObjectId(),
-  firstName: 'Vaniko',
-  lastName: 'Akopashvili',
-  token: 'secret_token',
-};
+const userId = faker.database.mongodbObjectId();
+const email = 'test@test.com';
 
 async function getUser(req, res) {
+  try {
+    const user = {
+      id: userId,
+      firstName: 'Vaniko',
+      lastName: 'Akopashvili',
+      email,
+      token: jwt.sign(
+        {
+          id: userId,
+          email,
+        },
+        'secret_key'
+      ),
+    };
+  } catch (err) {
+    console.log(err);
+  }
   res.status(200).send(transformSuccess(user));
 }
 
